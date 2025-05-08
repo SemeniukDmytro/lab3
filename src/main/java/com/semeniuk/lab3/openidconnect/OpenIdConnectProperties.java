6e8b61f87f3f27bd84d0da344c7a9283dc5b8bd9
Dmytro Semeniuk <dmytrosemeniuk31@gmail.com>
2025-05-08 00:29:55 -0400

    Initial commit

---
+++ src/main/java/com/semeniuk/lab3/openidconnect/OpenIdConnectProperties.java
@@ -0,0 +1,15 @@
+package com.semeniuk.lab3.openidconnect;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix = "casdoor")
+@Data
+public class OpenIdConnectProperties {
+
+    private String connectEndpoint;
+    private String connectClientId;
+    private String connectClientSecret;
+}
